1e305ae4df243fadca3fb7e1ff697280c9482376
Molinette505 <marc-alexandre.monette-molina.1@ens.etsmtl.ca>
2025-12-10 16:13:38 -0500

    Seperated css files and create color variables for other types

--- App/js/DataTypes.js
+++ App/js/DataTypes.js
@@ -1,14 +1,12 @@
 window.globalDataTypes = [
-    { "name": "exec",      "color": "var(--c-exec)",   "widget": "none" },
-    { "name": "boolean",   "color": "var(--c-bool)",   "widget": "checkbox" },
-    { "name": "float",     "color": "var(--c-float)",  "widget": "number" },
-    { "name": "string",    "color": "var(--c-string)", "widget": "text" },
-    { "name": "vector",    "color": "var(--c-vector)", "widget": "vector3" },
-    { "name": "color",     "color": "var(--c-color)",  "widget": "color" },
-    { "name": "int",       "color": "var(--c-int)",    "widget": "number" },
-    
-    // New Types
-    { "name": "class",     "color": "#5500FF",         "widget": "dropdown" },
-    { "name": "object",    "color": "#00A8FF",         "widget": "dropdown" },
-    { "name": "transform", "color": "#FF6600",         "widget": "none" }
+    { "name": "exec",      "color": "var(--c-exec)",      "widget": "none" },
+    { "name": "boolean",   "color": "var(--c-boolean)",   "widget": "checkbox" },
+    { "name": "float",     "color": "var(--c-float)",     "widget": "number" },
+    { "name": "string",    "color": "var(--c-string)",    "widget": "text" },
+    { "name": "vector",    "color": "var(--c-vector)",    "widget": "vector3" },
+    { "name": "color",     "color": "var(--c-color)",     "widget": "color" },
+    { "name": "int",       "color": "var(--c-int)",       "widget": "number" },
+    { "name": "class",     "color": "var(--c-class)",     "widget": "dropdown" },
+    { "name": "object",    "color": "var(--c-object)",    "widget": "dropdown" },
+    { "name": "transform", "color": "var(--c-transform)", "widget": "none" }
 ];
